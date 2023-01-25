3b8bef07ddce9eded6583cbdef374229664d4e24
Zach <zacharysabey@gmail.com>
2023-01-24 22:28:19 -0700

    New CCI and Income Data

--- zachEconIndicators.R
+++ zachEconIndicators.R
@@ -4,4 +4,5 @@ library(rvest)
 incomeData <- ("https://apps.bea.gov/iTable/?reqid=19&step=3&isuri=1&1921=survey&1903=58")
 
 disposable_income <- read_csv("Disposable_income.csv", skip = 3)
-  
+
+cciData <- read_csv("CCI.csv")
